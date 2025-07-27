f4bd3817b35ba3db12a6a9880d502bda84a1ac32
Samuel Adams <sama29571@gmail.com>
2025-07-27 16:18:22 +0000

    feat: add user posts to profile|

--- lib/posts/screens/user_posts.dart
+++ lib/posts/screens/user_posts.dart
@@ -1,3 +1,4 @@
+import 'package:cached_video_player_plus/cached_video_player_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:instagram/posts/repository/post_repository.dart';
@@ -23,12 +24,38 @@ class _UserPostsState extends ConsumerState<UserPosts> {
         } else if (snapshot.hasData) {
           final posts = snapshot.data!;
           print("posts $posts");
-          
+
           if (posts.isEmpty) {
             return Text("User has no posts");
           } else {
-            return Text("ahahahahaha");
-            // Or return your actual list view or grid view of posts here
+            return 
+            GridView.builder(
+          padding: EdgeInsets.all(10),
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3, // Number of columns
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          ),
+          itemCount: posts.length,
+          itemBuilder: (context, index) {
+            Map<String,dynamic> post=posts[index];
+            return Container(
+              color: Colors.red,
+              child: Center(
+                child: 
+                // Text(
+                //   '$index',
+                //   style: TextStyle(color: Colors.white, fontSize: 20),
+                // ),
+                // post["imageUrls"][0].endsWith('.jpg') || post["imageUrls"][0].endsWith('.jpeg')||post["imageUrls"][0].endsWith('.png')||post["imageUrls"][0].endsWith('.webp')? 
+                Image.network(post["imageUrls"][0])
+                // :
+              ),
+            );
+          },
+        )
+      ;
+
           }
         } else {
           return Text("Unexpected error");
