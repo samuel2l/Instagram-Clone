4b3884416c9addf61a996c383bdd0963386221f3
Samuel Adams <sama29571@gmail.com>
2025-08-01 01:23:58 +0000

    feat: add reel comments

--- lib/posts/screens/reels.dart
+++ lib/posts/screens/reels.dart
@@ -1,6 +1,7 @@
 import 'package:cached_video_player_plus/cached_video_player_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:instagram/auth/repository/auth_repository.dart';
 import 'package:instagram/posts/repository/post_repository.dart';
 
 class Reels extends ConsumerStatefulWidget {
@@ -14,12 +15,12 @@ class _ConsumerReelsState extends ConsumerState<Reels> {
   late PageController pageController;
   CachedVideoPlayerPlusController? controller;
   List<dynamic>? reelData;
-
+  TextEditingController commentController = TextEditingController();
   List<dynamic>? reels;
 
   int currentVideoIndex = 0;
   bool isHolding = false;
-
+  bool tapped = false;
   @override
   void initState() {
     super.initState();
@@ -154,13 +155,159 @@ class _ConsumerReelsState extends ConsumerState<Reels> {
                                             ),
                                         builder: (context, snapshot) {
                                           if (snapshot.hasData) {
-                                            return Text("${snapshot.data}",style: TextStyle(color: Colors.white),);
+                                            return Text(
+                                              "${snapshot.data}",
+                                              style: TextStyle(
+                                                color: Colors.white,
+                                              ),
+                                            );
                                           }
 
                                           return Text(" ");
                                         },
                                       ),
                                     ],
+                                  ),
+                                ),
+                                Positioned(
+                                  right: 0,
+                                  bottom: 150,
+                                  child: IconButton(
+                                    icon: Icon(Icons.message),
+                                    color: Colors.white,
+                                    onPressed: () {
+                                      showModalBottomSheet(
+                                        context: context,
+                                        isScrollControlled: true,
+                                        builder: (context) {
+                                          return DraggableScrollableSheet(
+                                            
+                                            initialChildSize: 0.5,
+                                            minChildSize: 0.5,
+                                            maxChildSize: 0.8,
+                                            expand: false,
+                                            builder: (
+                                              context,
+                                              scrollController,
+                                            ) {
+                                              return Container(
+                                                padding: EdgeInsets.all(16),
+                                                decoration: BoxDecoration(
+                                                  color: Colors.red,
+                                                  borderRadius:
+                                                      BorderRadius.vertical(
+                                                        top: Radius.circular(
+                                                          20,
+                                                        ),
+                                                      ),
+                                                ),
+                                                child: Column(
+
+                                                  children: [
+                                                    StreamBuilder(
+                                                      stream: ref
+                                                          .watch(
+                                                            postRepositoryProvider,
+                                                          )
+                                                          .getPostComments(
+                                                            reelData![index]["postId"],
+                                                          ),
+                                                      builder: (
+                                                        context,
+                                                        snapshot,
+                                                      ) {
+                                                        if (snapshot.hasData) {
+                                                          if (snapshot.data !=
+                                                                  null &&
+                                                              snapshot
+                                                                  .data!
+                                                                  .isNotEmpty) {
+                                                            final commentData =
+                                                                snapshot.data!;
+                                                            return Expanded(
+                                                              child: ListView.builder(
+                                                                itemCount:
+                                                                    snapshot
+                                                                        .data!
+                                                                        .length,
+                                                                itemBuilder: (
+                                                                  context,
+                                                                  index,
+                                                                ) {
+                                                                  final comment =
+                                                                      commentData[index];
+                                                                  return ListTile(
+                                                                    leading: CircleAvatar(
+                                                                      backgroundImage:
+                                                                          NetworkImage(
+                                                                            comment["dp"],
+                                                                          ),
+                                                                    ),
+                                                                    subtitle: Text(
+                                                                      "${comment["email"]}",
+                                                                    ),
+                                                                    title: Text(
+                                                                      "${comment["text"]}",
+                                                                    ),
+                                                                  );
+                                                                },
+                                                              ),
+                                                            );
+                                                          }
+                                                        }
+                                                        return Center(
+                                                          child: Text(
+                                                            "No comments yet",
+                                                          ),
+                                                        );
+                                                      },
+                                                    ),
+                                                    TextField(
+                                                      controller:
+                                                          commentController,
+                                                      onSubmitted: (
+                                                        value,
+                                                      ) async {
+                                                        final profileData =
+                                                            await ref
+                                                                .watch(
+                                                                  authRepositoryProvider,
+                                                                )
+                                                                .getUser();
+                                                        if (profileData !=
+                                                            null) {
+                                                          await ref
+                                                              .read(
+                                                                postRepositoryProvider,
+                                                              )
+                                                              .addCommentToPost(
+                                                                postId:
+                                                                    reelData![index]["postId"],
+                                                                email:
+                                                                    profileData
+                                                                        .email,
+                                                                dp:
+                                                                    profileData
+                                                                        .profile
+                                                                        .dp,
+                                                                commentText:
+                                                                    commentController
+                                                                        .text
+                                                                        .trim(),
+                                                              );
+                                                        }
+                                                        commentController
+                                                            .clear();
+                                                      },
+                                                    ),
+                                                  ],
+                                                ),
+                                              );
+                                            },
+                                          );
+                                        },
+                                      );
+                                    },
                                   ),
                                 ),
                               ],
@@ -198,12 +345,6 @@ class _ConsumerReelsState extends ConsumerState<Reels> {
                           size: 80,
                         ),
                       ),
-
-                    Positioned(
-                      right: 0,
-                      bottom: 150,
-                      child: Icon(Icons.message, color: Colors.white),
-                    ),
                   ],
                 ),
               )
