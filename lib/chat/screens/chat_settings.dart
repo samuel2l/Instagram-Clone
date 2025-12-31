4d05e99f4065369f179dcae5418ec7c9a9ba04b5
Samuel Adams <sama29571@gmail.com>
2025-12-31 16:03:35 +0000

    style: add chat settings UI

--- lib/chat/screens/chat_settings.dart
+++ lib/chat/screens/chat_settings.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:instagram/chat/repository/chat_repository.dart';
+import 'package:instagram/chat/screens/create_group.dart';
 
 class ChatSettings extends ConsumerStatefulWidget {
   const ChatSettings({super.key});
@@ -15,20 +16,111 @@ class _ChatSettingsState extends ConsumerState<ChatSettings> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body:  Center(
+      body: SingleChildScrollView(
         child: Column(
           children: [
             CircleAvatar(
               radius: 60,
-              backgroundImage: CachedNetworkImageProvider(ref.read(chatDataProvider)!.isGroup
-                  ? ref
-                      .read(chatDataProvider)
-                      !
-                      .dp
-                  : ref
-                      .read(messageRecipientProvider)!
-                      .profile
-                      .dp),
+              backgroundImage: CachedNetworkImageProvider(
+                ref.read(chatDataProvider)!.isGroup
+                    ? ref.read(chatDataProvider)!.dp
+                    : ref.read(messageRecipientProvider)!.profile.dp,
+              ),
+            ),
+            Text(
+              ref.read(chatDataProvider)!.isGroup
+                  ? ref.read(chatDataProvider)!.groupName!
+                  : ref.read(messageRecipientProvider)!.profile.name,
+
+              style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Text(
+                "change name and image",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                  color: Colors.blueAccent,
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(foregroundColor: Colors.black),
+                  child: Column(
+                    children: [
+                      Icon(Icons.person_add_outlined, size: 45),
+                      SizedBox(height: 10),
+                      Text("Add"),
+                    ],
+                  ),
+                ),
+                SizedBox(width: 10),
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(foregroundColor: Colors.black),
+                  child: Column(
+                    children: [
+                      Icon(Icons.person_remove_outlined, size: 45),
+                      SizedBox(height: 10),
+                      Text("Remove"),
+                    ],
+                  ),
+                ),
+                SizedBox(width: 10),
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(foregroundColor: Colors.black),
+                  child: Column(
+                    children: [
+                      Icon(Icons.edit_outlined, size: 45),
+                      SizedBox(height: 10),
+                      Text("Edit"),
+                    ],
+                  ),
+                ),
+                SizedBox(width: 10),
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(foregroundColor: Colors.black),
+                  child: Column(
+                    children: [
+                      Icon(Icons.person_outline, size: 45),
+                      SizedBox(height: 10),
+                      Text("Members"),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            ListTile(
+              leading: Icon(Icons.info_outline_rounded),
+              title: Text("Something isn't working"),
+              trailing: Icon(Icons.arrow_forward_ios_outlined),
+            ),
+            ListTile(
+              leading: Icon(Icons.lock_outline_rounded),
+              title: Text("Privacy and safety"),
+              trailing: Icon(Icons.arrow_forward_ios_outlined),
+            ),
+
+            ListTile(
+              onTap: (){
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => CreateGroup(),
+                  ),
+                );  
+              },
+              leading: Icon(Icons.group_add),
+              title: Text("Create a new group chat"),
+              trailing: Icon(Icons.arrow_forward_ios_outlined),
             ),
           ],
         ),
